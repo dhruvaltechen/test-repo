efc6b21b529b728eecea277605ac1f97a9bd06e8
sandeep shah <sandeep@techeniac.com>
2025-07-04 13:50:29 +0530

    test2-development

---
+++ test122.js
@@ -0,0 +1,12 @@
+/*************  ✨ Windsurf Command ⭐  *************/
+var date1 = new Date("07/13/2010");
+var date2 = new Date("12/15/2010");
+
+if (date1 < date2) {
+  alert("date1 is earlier than date2");
+} else if (date1 > date2) {
+  alert("date1 is later than date2");
+} else {
+  alert("The two dates are the same");
+}
+/*******  3e89d098-1995-4b26-8f9f-fc20c45b4139  *******/

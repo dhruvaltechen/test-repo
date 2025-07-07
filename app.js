b405579d3305b0767ae0673cc083ddd9b4e87c8b
dhruvaltechen <dhruval@techeniac.com>
2025-07-07 11:59:44 +0530

    Feature: Update greet and add farewell function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function greet(name) {
-    return `Hello, ${name}!`;
+    return `Hello, ${name}! Welcome to the app!`;
+}
+
+function farewell(name) {
+    return `Goodbye, ${name}!`;
 }
 
 console.log(greet("World"));
+console.log(farewell("World"));
